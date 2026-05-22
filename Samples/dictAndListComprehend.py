28c8b55b229b0e40edebad6acf13bf199ed10211
Brian Donovan <bdonovan0726@gmail.com>
2026-05-21 22:58:37 -0500

    more cleanup

--- Samples/dictAndListComprehend.py
+++
@@ -1,7 +0,0 @@
-from Data.SQLite import SQLiteconn
-
-with SQLiteconn("C:\sources\BTDWA\Data\weather.db") as SQConn:
-    obs, desc = SQConn.getAllCachedNWSObservations()
-    print(enumerate(desc))
-    print(desc)
-    
